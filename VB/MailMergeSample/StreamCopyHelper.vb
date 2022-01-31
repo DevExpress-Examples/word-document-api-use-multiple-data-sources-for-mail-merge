6b1cb0518a7cdb6fb4da7a600897742b0677e36c
DevExpressExampleBot <fakeEmail>
2022-01-31 11:46:40 +0300

    Source auto update [skip ci]

--- VB/MailMergeSample/StreamCopyHelper.vb
+++ VB/MailMergeSample/StreamCopyHelper.vb
@@ -1,14 +1,14 @@
-﻿Imports System.IO
+Imports System.IO
 
 Public Module StreamCopyHelper
-	Public Sub Copy(ByVal src As Stream, ByVal dst As Stream)
-		Const bufferSize As Integer = 32768
-		Dim buffer(bufferSize - 1) As Byte
-		Dim bytesRead As Integer = 0
-		Do
-			bytesRead = src.Read(buffer, 0, bufferSize)
-			dst.Write(buffer, 0, bytesRead)
 
-		Loop While bytesRead = bufferSize
-	End Sub
+    Public Sub Copy(ByVal src As Stream, ByVal dst As Stream)
+        Const bufferSize As Integer = 32768
+        Dim buffer As Byte() = New Byte(32767) {}
+        Dim bytesRead As Integer = 0
+        Do
+            bytesRead = src.Read(buffer, 0, bufferSize)
+            dst.Write(buffer, 0, bytesRead)
+        Loop While bytesRead = bufferSize
+    End Sub
 End Module
